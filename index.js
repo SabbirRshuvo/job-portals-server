117eabab00dc448e5da3a8d88750843aaecfdb38
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2025-03-19 23:38:00 +0600

    project setup is done

--- index.js
+++ index.js
@@ -3,11 +3,33 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorize" });
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: "unoauthorize" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nugjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -36,6 +58,28 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: "5h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
     app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -60,7 +104,6 @@ async function run() {
       const application = req.body;
       const result = await jobApplication.insertOne(application);
       res.send(result);
-
       const id = application.job_id;
       const query = { _id: new ObjectId(id) };
       const job = await jobsCollection.findOne(query);
@@ -82,13 +125,16 @@ async function run() {
       res.send(job);
     });
 
-    app.get("/job_application", async (req, res) => {
+    app.get("/job_application", verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email };
+      console.log(req.cookies?.token);
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ message: "forbiden" });
+      }
       const result = await jobApplication.find(query).toArray();
 
       for (const application of result) {
-        console.log(application.job_id);
         const query1 = { _id: new ObjectId(application.job_id) };
         const job = await jobsCollection.findOne(query1);
         if (job) {
